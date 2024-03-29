2314ea4d5d783d815bc0f6b1a8315139e8bd1025
Guillermo Díaz Romero <guillermomigueltercero@gmail.com>
2024-03-29 12:38:42 -0300

    Tercer commit (deberia haber sido el segundo)

---
+++ tip1_1.c
@@ -0,0 +1,18 @@
+// Punto 3
+#include <stdio.h>
+
+void main (){
+    printf("Hola mundo");   
+
+
+    //Declaracion de puntero a una variable tipo entero
+    int variable=5;
+    int *pvariable = &variable;
+
+    printf("\nContenido del puntero: %d",*pvariable);
+    printf("\nDireccion de memoria almacenada del puntero: %p",pvariable);
+    printf("\nDireccion de memoria de la variable: %p",&variable);
+    printf("\nLa direccion de memoria del puntero: %p", &pvariable);
+    printf("\nMemoria ultilizada %d", sizeof(variable));
+
+}
